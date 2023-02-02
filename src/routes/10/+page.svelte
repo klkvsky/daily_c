b48d43b4d6274a68e1886618549a5cbc4d841053
Kulikovsky <80684329+klkvsky@users.noreply.github.com>
2023-02-02 12:15:54 +0500

    fix

--- src/routes/10/+page.svelte
+++ src/routes/10/+page.svelte
@@ -14,7 +14,7 @@
 	$: y >= 900 && (y = 900);
 </script>
 
-<div class="bg-neutral-100 w-screen h-[200vh] grid place-items-center">
+<div class="bg-neutral-100 w-screen h-[400vh] grid place-items-center">
 	<div
 		class="fixed top-[50%] left-[50%]"
 		style={`transform: translate(-50%, -50%) rotate(${y / 2}deg) scale(${y / 1500 + 1}) ;`}
@@ -24,13 +24,14 @@
 				{#each people as person, index}
 					<div
 						class="w-full h-full relative "
-						style={`grid-area: ${GRID_AREAS[index]}; overflow: hidden !important; border-radius: 15px !important; position: relative !important;`}
+						style={`grid-area: ${GRID_AREAS[index]}; overflow: hidden !important; border-radius: 5px !important; position: relative !important;`}
 					>
 						<div class="w-full h-full" style={`transform: rotate(-${y / 2}deg);`}>
 							<video
 								src={person.video}
 								autoplay
 								muted
+								loop
 								class="w-full h-full bg-red-500 object-cover scale-[2]"
 							/>
 						</div>
